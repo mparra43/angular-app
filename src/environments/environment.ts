fe65bfa556a8c1788259f1128880d6673b508fae
mparra43 <66500440+mparra43@users.noreply.github.com>
2023-08-30 09:00:42 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   api: 'https://api.themoviedb.org/3',
   apiUser:'http://localhost:8081/api/v1',
-  userApi:'Lorena.parra',
-  account:'20355535',
-  token: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NmI2NDcxZmM3NGQyNzYwYTdhNmY4ZDI5MTRlNDQ1MyIsInN1YiI6IjY0ZTk1YjBhNDU4MTk5MDBhZGE5NDM2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sWHglT2To6RLTu8YlYOcSyF-1-7Rg1qabv0jJ1fXqVo'
+  userApi:'',
+  account:'',
+  token: ''
 };
